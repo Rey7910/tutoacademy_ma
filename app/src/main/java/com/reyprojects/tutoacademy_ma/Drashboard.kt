0b4a2ca6b8b25dd63d2f94e2691008504cca4e7d
John Doe <jpenafielo@unal.edu.co>
2023-05-06 18:45:09 -0500

    Implemented correctly topBar and Drawer

--- app/src/main/java/com/reyprojects/tutoacademy_ma/Drashboard.kt
+++ app/src/main/java/com/reyprojects/tutoacademy_ma/Drashboard.kt
@@ -1,31 +1,32 @@
 package com.reyprojects.tutoacademy_ma
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import android.widget.Toast
 import androidx.compose.foundation.Image
-
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-
 import androidx.compose.material.icons.filled.Menu
-
 import androidx.compose.runtime.Composable
-
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 
@@ -45,17 +46,24 @@ class MainActivity : ComponentActivity() {
 fun PantallaPrincipal(){
     val scaffoldState = rememberScaffoldState()
     val scope = rememberCoroutineScope()
-    val navigationItems = listOf(
-        Destinos.Pantalla1,
-        Destinos.Pantalla2,
-        Destinos.Pantalla3,
-        Destinos.Pantalla4
-    )
+    val contextForToast = LocalContext.current.applicationContext
 
     Scaffold(
+
         scaffoldState = scaffoldState,
         topBar = {TopBar(scope, scaffoldState)},
-        drawerContent = {Drawer(menu_items = navigationItems)}
+        drawerContent = {  DrawerContent { itemLabel ->
+            Toast
+                .makeText(contextForToast, itemLabel, Toast.LENGTH_SHORT)
+                .show()
+            scope.launch {
+                // delay for the ripple effect
+                delay(timeMillis = 250)
+                scaffoldState.drawerState.close()
+            }
+        }
+        },
+        backgroundColor = colorResource(id = R.color.background)
     ){ it
 
     }
@@ -68,58 +76,156 @@ fun TopBar(
     scaffoldState: ScaffoldState
 ){
     TopAppBar (
-        title = { Text(text = "TutoAcademy")},
+        title = { Text(text = "TutoAcademy", color = Color.Black)},
+        backgroundColor = colorResource(id=R.color.white),
         navigationIcon = {
             IconButton(onClick = {
                 scope.launch { scaffoldState.drawerState.open() }
             }) {
                 Icon(imageVector = Icons.Filled.Menu, contentDescription = "Icono de Menú")
             }
+        },
+        actions = {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = null,
+            )
         }
     )
 }
 
 @Composable
-fun Drawer(menu_items: List<Destinos>){
-    Image(painterResource(id = R.drawable.chayanne),
-        contentDescription = "Chayanne :D",
-        contentScale = ContentScale.Crop,
+private fun DrawerContent(
+    itemClick: (String) -> Unit
+) {
+
+    val itemsList = prepareNavigationDrawerItems()
+
+    LazyColumn(
         modifier = Modifier
-            .size(200.dp)
-            .height(200.dp)
-            .padding(20.dp)
-            .fillMaxWidth()
-            .clip(CircleShape),
+            .fillMaxSize()
+            .background(colorResource(id = R.color.background_drawer)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        contentPadding = PaddingValues(vertical = 36.dp)
+    ) {
 
-        )
-    Column(){
-        menu_items.forEach {item ->
+        item {
 
-            DrawerItem(item = item)
+            // user's image
+            Image(
+                modifier = Modifier
+                    .size(size = 120.dp)
+                    .clip(shape = CircleShape),
+                painter = painterResource(id = R.drawable.chayanne),
+                contentDescription = "Profile Image"
+            )
 
+            // user's name
+            Text(
+                modifier = Modifier
+                    .padding(top = 12.dp),
+                text = "Bryan Smith Colorado Lopez",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+            )
+
+            // user's email
+            Text(
+                modifier = Modifier.padding(top = 8.dp, bottom = 30.dp),
+                text = "bcolorado@unal.edu.co",
+                fontWeight = FontWeight.Normal,
+                fontSize = 16.sp,
+            )
+
+
+        }
+
+        items(itemsList) { item ->
+            NavigationListItem(item = item) {
+                itemClick(item.label)
+            }
         }
     }
 }
-
 @Composable
-fun DrawerItem(item: Destinos){
-    Row(
-
+private fun NavigationListItem(
+    item: NavigationDrawerItem,
+    itemClick: () -> Unit
+) {
+    TextButton(
         modifier = Modifier
             .fillMaxWidth()
-            .height(56.dp)
-            .padding(15.dp)
-            .clip(RoundedCornerShape(12))
+            .clickable {
+                itemClick()
+            },
+        onClick = {}
+    ) {
 
-    ){
-        Icon(
-            painter = painterResource(R.drawable.ic_profile),
-            contentDescription = "sex",
-            Modifier.size(24.dp)
-        )
-        Spacer(modifier = Modifier.width(24.dp))
-        Text(text= item.title,
-            style = MaterialTheme.typography.body1)
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(15.dp)
+        ) {
+            Icon(
+                modifier = Modifier
+                    .size(size = 28.dp),
+                painter = item.image,
+                contentDescription = null,
+                tint = colorResource(id = R.color.black)
+            )
+
+
+
+            // label
+            Text(
+                modifier = Modifier.padding(start = 16.dp),
+                text = item.label,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Medium,
+                color = colorResource(id = R.color.black)
+            )
+        }
+
+
     }
-
 }
+@Composable
+private fun prepareNavigationDrawerItems(): List<NavigationDrawerItem> {
+    val itemsList = arrayListOf<NavigationDrawerItem>()
+
+    itemsList.add(
+        NavigationDrawerItem(
+            image = painterResource(id = R.drawable.begin),
+            label = "Inicio"
+        )
+    )
+    itemsList.add(
+        NavigationDrawerItem(
+            image = painterResource(id = R.drawable.ic_profile),
+            label = "Perfil"
+        )
+    )
+    itemsList.add(
+        NavigationDrawerItem(
+            image = painterResource(id = R.drawable.calendar),
+            label = "Mis tutorias"
+        )
+    )
+    itemsList.add(
+        NavigationDrawerItem(
+            image = painterResource(id = R.drawable.chat),
+            label = "Chats",
+        )
+    )
+    itemsList.add(
+        NavigationDrawerItem(
+            image = painterResource(id = R.drawable.logout),
+            label = "Cerrar Sesión"
+        )
+    )
+
+    return itemsList
+}
+
+data class NavigationDrawerItem(
+    val image: Painter,
+    val label: String,
+)
