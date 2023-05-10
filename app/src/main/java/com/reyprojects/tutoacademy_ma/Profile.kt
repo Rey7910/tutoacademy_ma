46a79327fed283ba21bd1297b2bfe4db201e5548
Mafs06 <mcabrales@unal.edu.co>
2023-05-09 21:34:22 -0500

    version difference resolved

--- app/src/main/java/com/reyprojects/tutoacademy_ma/Profile.kt
+++ app/src/main/java/com/reyprojects/tutoacademy_ma/Profile.kt
@@ -1,12 +1,71 @@
 package com.reyprojects.tutoacademy_ma
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun Profile(){
-    Column {
-        Text("Aqui va el perfil")
+    Column(
+        modifier = Modifier.padding(20.dp)
+    ){
+        ProfileName("Jessica")
+        Spacer(modifier = Modifier.height(10.dp))
+        ProfileInfo()
+        Spacer(modifier = Modifier.height(10.dp))
+        ProfileSkills()
+        Spacer(modifier = Modifier.height(10.dp))
+        ProfileTutoringSchedule()
     }
+}
+@Composable
+fun ProfileName(name: String) {
+    Text(text = "Hola " + "$name" + "!",
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold)
+}
+
+@Composable
+fun ProfileInfo() {
+    Column(modifier = Modifier
+        .background(colorResource(id = R.color.light_blue))
+        .padding(15.dp)) {
+        Text("Información general:", fontSize = 20.sp)
+        Spacer(modifier = Modifier.height(5.dp))
+        Text("Genero: ")
+        Text("Fecha de nacimiento: ")
+        Text("Nacionalidad: ")
+        Text("Estudios: ")
+        Text("Descripción: ")
+    }
+}
+
+@Composable
+fun ProfileSkills(){
+    Text("Habilidades: ", fontSize = 20.sp)
+}
+
+@Composable
+fun ProfileTutoringSchedule(){
+    Text("Horario de tutorías: ", fontSize = 20.sp)
+}
+
+@Composable
+@Preview(showBackground = true)
+fun ProfileScreen(){
+    Profile()
 }
